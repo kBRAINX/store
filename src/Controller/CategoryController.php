361049806380f5abb3f5210ac2bb7e440879f8ac
KBRAINX <aurel.nguepssi@facsciences-uy1.cm>
2024-08-04 07:15:56 +0100

    permission

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -11,10 +11,12 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 
 
+#[IsGranted('ROLE_SUPER_ADMIN')]
 class CategoryController extends AbstractController
 {
     #[Route('/api/categories', methods: ['GET'])]
