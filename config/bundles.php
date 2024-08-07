5679ce4e6512edee5ca1f993568d737c0bedc21f
KBRAINX <aurel.nguepssi@facsciences-uy1.cm>
2024-08-07 15:58:05 +0100

    cors configuration

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
